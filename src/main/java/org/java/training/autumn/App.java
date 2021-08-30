465268caa54b3127d866ecc915400b8b40874af0
samovaroparovozovetralet <sashakrisk@gmail.com>
2021-08-30 21:36:20 +0300

    basic project commit

---
+++ src/main/java/org/java/training/autumn/App.java
@@ -0,0 +1,13 @@
+package org.java.training.autumn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
