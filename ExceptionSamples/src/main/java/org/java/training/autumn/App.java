4abb9564c9daa1e5ddcc9b137d2d9c31317e8c8c
samovaroparovozovetralet <sashakrisk@gmail.com>
2021-09-10 04:04:25 +0300

    part 1 exception block 1-6 with testing

--- ExceptionSamples/src/main/java/org/java/training/autumn/App.java
+++ ExceptionSamples/src/main/java/org/java/training/autumn/App.java
@@ -8,11 +8,11 @@ public class App
 {
 
     // syntax examples
-    public static void syntaxExample1() throws Throwable { }
+    public static void throwsSyntaxExample() throws Throwable { }
 
-    public static void syntaxExample2(){ try { }catch (Throwable t){} }
+    public static void tryCatchSyntaxExample(){ try { }catch (Throwable t){} }
 
-    public static void syntaxExample3(){
+    public static void throwSyntaxExample(){
         throw new Error();
     }
 
@@ -20,4 +20,139 @@ public class App
     public static void tryThrowNull(){
         throw null; // NullPointerException
     }
+
+    // recursive throw call end up in StackOverflowError
+    public static void throwRecursiveNull(NullPointerException e){
+        try { throw e; } catch (NullPointerException npe){throwRecursiveNull(npe);}
+    }
+
+    // System.out is buffered so Error appears in the middle of stream
+    public static void printWithBuffer(){
+        long start = System.currentTimeMillis();
+        for (int i=0; i<1000000;i++){
+            System.out.println(i);
+        }
+        System.out.println(System.currentTimeMillis() - start);
+        throw new Error();
+    }
+
+    // method without return statement
+    public static int methodWithoutReturn(){
+        throw new Error();
+    }
+
+    // handling wrong params
+    public static int area(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Negative sizes: w = " + width + ", h = " + height);
+        }
+        return width * height;
+    }
+
+    // try catch with method stack
+
+    public static String firstOfStack(){
+        StringBuilder sb = new StringBuilder("1");
+        try{second(sb);}
+        catch(Error e) {
+            sb.append("Catch");
+        }
+        sb.append("Out");
+        return sb.toString();
+    }
+
+    private static void second(StringBuilder sb){
+        sb.append("2");
+        third(sb);
+        sb.append("2");
+    }
+
+    private static void third(StringBuilder sb){
+        sb.append("3");
+        if (true){
+            sb.append("Throw");
+            throw new Error();
+        }
+        sb.append("3");
+    }
+
+    // exception polymorphism
+
+    // Exception catches RuntimeException
+    public static int catchRuntimeExceptionWithException(){
+        int x =1;
+        try{
+            if (true){throw new RuntimeException();}
+            x *=2;
+        }
+        catch (Exception e){
+            x*=3;
+        }
+        x*=5;
+        return x; // x==15
+    }
+
+    // can`t catch parent with child
+    public static void catchParentWithChild() throws Exception{
+        int x =1;
+        try{
+            if (true){throw new Exception();}
+            x *=2;
+            System.out.println(x);
+        }
+        catch (RuntimeException e){
+            x*=3;
+            System.out.println(x);
+        }
+        x*=5;
+        System.out.println(x);
+    }
+
+    // can`t catch from other catch
+    public static void catchingOnlyFromTry(){
+        int x =1;
+        try{
+            if (true){throw new RuntimeException();}
+        }
+        catch (RuntimeException e){
+            x*=2;
+            System.out.println(x);
+            if (true){throw new Error();}
+        }
+        catch(Error err){
+            x*=3;
+            System.out.println(x);
+        }
+        x*=5;
+        System.out.println(x);
+    }
+
+    // only first catch
+    public static int firstToCatch(){
+        int x=1;
+        try{
+            throw new Exception();
+        }
+        catch (RuntimeException re){x*=2;}
+        catch(Exception e){x*=3;}
+        catch(Throwable t){x*=5;}
+        return x; // x==3
+    }
+
+    // catches actual object type not reference type
+    public static int catchingObjectNotReference(){
+        int x=1;
+        try{
+            Throwable t = new Exception();
+            throw t;
+        }
+        catch (RuntimeException re){x*=2;}
+        catch(Exception e){x*=3;}
+        catch(Throwable t){x*=5;}
+        return x; // x==3
+    }
+
+    // finally
+
+    // finally block executes if
 }
--- ExceptionSamples/src/test/java/org/java/training/autumn/AppTest.java
+++ ExceptionSamples/src/test/java/org/java/training/autumn/AppTest.java
@@ -2,9 +2,11 @@ package org.java.training.autumn;
 
 
 
-import static org.java.training.autumn.App.tryThrowNull;
+import static org.java.training.autumn.App.*;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -12,8 +14,71 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    // syntax testing
+    @Test(expected=Error.class)
+    public void testThrow(){
+        throwSyntaxExample();
+    }
+
     @Test(expected=NullPointerException.class)
     public void testThrowNull(){
          tryThrowNull();
     }
+
+    @Test(expected=StackOverflowError.class)
+    public void testRecursiveNullThrow(){
+        throwRecursiveNull(null);
+    }
+
+    @Test(expected=Error.class)
+    public void testMethodWithoutReturn(){
+        int defaultInt = methodWithoutReturn();
+        System.out.println(defaultInt); // this line never reached
+    }
+
+    @Test
+    public void testTryCatchWithMethodStack(){
+        String s = "123ThrowCatchOut";
+        Assert.assertTrue(s.equals(firstOfStack()));
+    }
+
+    // test exception polymorphism
+
+    @Test
+    public void testCatchRuntimeExceptionWithException(){
+        catchRuntimeExceptionWithException();
+        Assert.assertTrue(catchRuntimeExceptionWithException()==15);
+    }
+
+    @Ignore
+    @Test
+    public void testCatchParentWithChild(){
+        int x=-1;
+        try{
+            catchParentWithChild();
+        }
+        catch(Exception e){Assert.assertTrue(x==-1);}
+    }
+
+    @Ignore
+    @Test(expected=Error.class)
+    public void testCatchOnlyFromTry(){
+        catchingOnlyFromTry();
+    }
+
+    @Test
+    public void testFirstToCatch(){
+        int x = firstToCatch();
+        Assert.assertEquals(x,3);
+    }
+
+    @Test
+    public void testCatchingObjectNotReference(){
+        int x = catchingObjectNotReference();
+        Assert.assertEquals(x, 3);
+    }
+
+    // finally testing
+
+
 }
