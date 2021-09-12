1d0c09093c37c2c0fdcfd15d822dccaeb6d2464a
samovaroparovozovetralet <sashakrisk@gmail.com>
2021-09-12 19:31:27 +0300

    added Person class and DB of Persons

---
+++ SalaryRecalculation/src/main/java/org/java/training/autumn/Person.java
@@ -0,0 +1,52 @@
+package org.java.training.autumn;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public final class Person implements Comparable<Person> {
+    private String firstName;
+    private String secondName;
+    private LocalDate birthDate;
+    private int hashCode;
+
+    public Person(PersonDB noteBook){
+        this.firstName = noteBook.getFirstName();
+        this.secondName = noteBook.getSecondName();
+        this.birthDate = noteBook.getBirthDate();
+        hashCode = hashCode();
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getSecondName(){
+        return secondName;
+    }
+
+    public LocalDate getBirthDate(){
+        return birthDate;
+    }
+
+    public int compareTo(Person other){
+        return secondName.compareTo(other.getFirstName());
+    }
+
+    public int hashCode(){
+        int prime = 101;
+        int out = prime * firstName.hashCode() + secondName.hashCode();
+        out = out * prime + birthDate.hashCode();
+        return out;
+    }
+
+    public boolean equals(Object o){
+        if(o==null){return false;}
+        if(this.getClass()!=o.getClass()){return false;}
+        Person that = (Person) o;
+        if(hashCode!=that.hashCode){return false;}
+        if(!firstName.equals(that.getFirstName())){return false;}
+        if(!secondName.equals(that.getSecondName())){return false;}
+        if(!birthDate.equals(that.birthDate)){return false;}
+        return true;
+    }
+}
---
+++ SalaryRecalculation/src/main/java/org/java/training/autumn/PersonDB.java
@@ -0,0 +1,27 @@
+package org.java.training.autumn;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public enum PersonDB {
+    PERSON_ONE("Oleksandr", "Krysynskyi", "1996-19-04"),
+    PERSON_TWO("Taras", "Shevchenko", "1814-03-09");
+
+    private final String firstName;
+    private final String secondName;
+    private final LocalDate birthDate;
+
+    PersonDB(String firstName, String secondName, String birthDate){
+        Objects.requireNonNull(firstName);
+        Objects.requireNonNull(secondName);
+        Objects.requireNonNull(birthDate);
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.birthDate = LocalDate.parse(birthDate);
+    }
+
+    public String getFirstName(){return firstName;}
+    public String getSecondName(){return secondName;}
+    public LocalDate getBirthDate(){return birthDate;}
+
+}
