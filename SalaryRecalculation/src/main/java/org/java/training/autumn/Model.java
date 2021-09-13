ef7f9bae4a7e1f5f09ca3d2717222eab60cbf5c1
samovaroparovozovetralet <sashakrisk@gmail.com>
2021-09-13 14:42:40 +0300

    added transfer methods

--- SalaryRecalculation/src/main/java/org/java/training/autumn/Model.java
+++ SalaryRecalculation/src/main/java/org/java/training/autumn/Model.java
@@ -45,11 +45,17 @@ public class Model {
     }
 
     public void remove(Employee employee){
+        for(Connectable connection : employee.getConnections()){
+            connection.getDisconnectedFrom(employee);
+        }
         companyEmployees.remove(employee);
     }
 
     public void remove(Manager manager){
-        // TODO: how to reassign related employees
+        // TODO: whom to reassign related employees
+        for(Connectable connection : manager.getConnections()){
+            connection.getDisconnectedFrom(manager);
+        }
         companyEmployees.remove(manager);
     }
 
@@ -57,15 +63,54 @@ public class Model {
             throws AlreadyConnectedException {
         Objects.requireNonNull(manager);
         manager.getConnectedTo(employee);
+        employee.getConnectedTo(manager);
     }
 
     public void removeEmployeeAssignment(Manager manager, Employee employee){
         Objects.requireNonNull(manager);
         manager.getDisconnectedFrom(employee);
+        employee.getDisconnectedFrom(manager);
     }
 
     public void transferToNewPosition(Employee employee, int salary){
+        Objects.requireNonNull(employee);
+        Employee transferredEmployee = new Employee(employee.getPerson(),
+                LocalDate.now(), salary);
         remove(employee);
+        try{
+            addEmployee(transferredEmployee);
+        }
+        catch (EmployeeAlreadyExistsException e){
+            throw new Error("Multiple instances of single employee in DB");
+        }
+    }
+
+    public void transferToNewPosition(Employee employee, int salary,
+                                      ArrayList<Employee> managerAssignedEmployees){
+        Objects.requireNonNull(employee);
+        Manager transferredEmployee = new Manager(employee.getPerson(),
+                LocalDate.now(), salary, managerAssignedEmployees);
+        remove(employee);
+        try{
+            addEmployee(transferredEmployee);
+        }
+        catch (EmployeeAlreadyExistsException e){
+            throw new Error("Multiple instances of single employee in DB");
+        }
+    }
+
+    public void transferToNewPosition(Employee employee, int salary,
+                                      String jobDescription){
+        Objects.requireNonNull(employee);
+        OtherEmployee transferredEmployee = new OtherEmployee(employee.getPerson(),
+                LocalDate.now(), salary, jobDescription);
+        remove(employee);
+        try{
+            addEmployee(transferredEmployee);
+        }
+        catch (EmployeeAlreadyExistsException e){
+            throw new Error("Multiple instances of single employee in DB");
+        }
     }
 
     //utility method
