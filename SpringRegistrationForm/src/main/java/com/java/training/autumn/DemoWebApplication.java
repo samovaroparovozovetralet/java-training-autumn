5209e7c9ed39fc7d6c654f3e6c604803a57cec2d
samovaroparovozovetralet <sashakrisk@gmail.com>
2021-09-27 10:22:21 +0300

    RegistrationForm with spring started

---
+++ SpringRegistrationForm/src/main/java/com/java/training/autumn/DemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.java.training.autumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoWebApplication.class, args);
+	}
+
+}
